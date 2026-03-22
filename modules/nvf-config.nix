7ec5d203ec4015a4a95ee64fe0003b7eea711bad
Jose Vega <josevega234@gmail.com>
2026-03-22 05:09:23 +0100

    nvim tmux

--- modules/nvf-config.nix
+++ modules/nvf-config.nix
@@ -19,7 +19,38 @@
       vim.autocomplete.blink-cmp.enable = true;
       vim.autopairs.nvim-autopairs.enable = true;
       vim.filetree.nvimTree.enable = true;
+      vim.formatter.conform-nvim.enable = true;
       vim.binds.whichKey.enable = true;
+        vim.lazy.plugins.vim-tmux-navigator = {
+            package = pkgs.vimPlugins.vim-tmux-navigator;
+            event = ["VimEnter"];
+            keys = [
+                    {
+                        key = "<C-h>";
+                        mode = [ "n" ];
+                        action = "<CMD><C-U>TmuxNavigateLeft<CR>";
+                        desc = "Focus Left";
+                    }
+                    {
+                        key = "<C-l>";
+                        mode = [ "n" ];
+                        action = "<CMD><C-U>TmuxNavigateRight<CR>";
+                        desc = "Focus Right";
+                    }
+                    {
+                        key = "<C-j>";
+                        mode = [ "n" ];
+                        action = "<CMD><C-U>TmuxNavigateDown<CR>";
+                        desc = "Focus Down";
+                    }
+                    {
+                        key = "<C-k>";
+                        mode = [ "n" ];
+                        action = "<CMD><C-U>TmuxNavigateUp<CR>";
+                        desc = "Focus Up";
+                    }
+            ];
+          };
     };
   };
 
