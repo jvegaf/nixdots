da6dd84e2beeb06cd31812c7e03fb6e6dde02760
Jose Vega <josevega234@gmail.com>
2026-04-14 23:59:47 +0200

    fixes

--- modules/services.nix
+++ modules/services.nix
@@ -2,7 +2,6 @@
 {
   programs.ssh.startAgent = true;
   programs.firefox.enable = true;
-  programs.zsh.enable = true;
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
--- modules/user.nix
+++ modules/user.nix
@@ -1,11 +1,14 @@
 { pkgs, ... }:
 {
+  programs.zsh.enable = true;
+
   users.users.th3g3ntl3man = {
     isNormalUser = true;
     description = "The Gentleman";
     extraGroups = [
       "networkmanager"
       "wheel"
+      "input"
     ];
     shell = pkgs.zsh;
     # packages = with pkgs; [
